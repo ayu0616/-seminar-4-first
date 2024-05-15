e960e636036820628c6f789cec287bcdc097b45b
ayu0616 <ayumuogawa0616@gmail.com>
2024-05-15 16:23:09 +0900

    音節を定義した

--- src/type/phoneme.py
+++ src/type/phoneme.py
@@ -12,3 +12,13 @@ SYLLABIC_NASAL = Mora(vowel="", consonant="N")  # 撥音
 SOKUON = Mora(vowel="", consonant="Q")  # 促音
 
 
+class SYLLABLE(BaseModel):
+    """音節"""
+
+    onset: list[Mora]  # 音節頭
+    syllabic: list[Mora]  # 音節主音
+    coda: list[Mora]  # 末尾子音
+
+    @property
+    def mora_list(self) -> list[Mora]:
+        return self.onset + self.syllabic + self.coda
