89981b0e5252cd1750f66c56c78d3ca1147993e9
ayu0616 <ayumuogawa0616@gmail.com>
2024-06-18 19:37:37 +0900

    系列SVMを実装した

--- src/type/features.py
+++ src/type/features.py
@@ -5,12 +5,25 @@ from pydantic import BaseModel
 from type import Abbreviation
 
 
+class CrfLabel(BaseModel):
+    NG: str = "NG"
+    B_ABBR: str = "B-Abbr"
+    I_ABBR: str = "I-Abbr"
+
+
+crf_label = CrfLabel()
+
+
 class CrfFeatures(BaseModel):
+    BOS: ClassVar[str] = "BOS"
     EOS: ClassVar[str] = "EOS"
+    to_int_idx: ClassVar[dict[str, int]] = {v: i for i, v in enumerate(crf_label.model_dump().values())}
 
     # モーラ
     vowel: str
     consonant: str
+    prev_vowel: str
+    prev_consonant: str
     next_vowel: str
     next_consonant: str
     is_syllabic_nasal: bool  # 撥音かどうか
@@ -33,49 +46,88 @@ class CrfFeatures(BaseModel):
         elem_len = len(abbr.word_element_list)
         for i, elem in enumerate(abbr.word_element_list):
             mora_len_in_elem = len(elem.mora_list)
-            for j, mora in enumerate(elem.mora_list):
+            for j, feat_list in enumerate(elem.mora_list):
                 if j == mora_len_in_elem - 1:
                     next_vowel = cls.EOS
                     next_consonant = cls.EOS
                 else:
                     next_vowel = elem.mora_list[j + 1].vowel
                     next_consonant = elem.mora_list[j + 1].consonant
+                if j == 0:
+                    prev_vowel = cls.BOS
+                    prev_consonant = cls.BOS
+                else:
+                    prev_vowel = elem.mora_list[j - 1].vowel
+                    prev_consonant = elem.mora_list[j - 1].consonant
                 res.append(
                     cls(
-                        vowel=mora.vowel,
-                        consonant=mora.consonant,
+                        vowel=feat_list.vowel,
+                        consonant=feat_list.consonant,
+                        prev_vowel=prev_vowel,
+                        prev_consonant=prev_consonant,
                         next_vowel=next_vowel,
                         next_consonant=next_consonant,
                         elem_num=i,
                         mora_num=j,
                         elem_len=elem_len,
                         mora_len_in_elem=mora_len_in_elem,
-                        is_syllabic_nasal=mora.is_syllabic_nasal(),
-                        is_sokuon=mora.is_sokuon(),
+                        is_syllabic_nasal=feat_list.is_syllabic_nasal(),
+                        is_sokuon=feat_list.is_sokuon(),
                     )
                 )
+        # 特徴量のインデックスを取得
+        for feat_list in res:
+            for k, v in feat_list.model_dump().items():
+                if type(v) in {int, float, bool}:
+                    feat = k
+                else:
+                    feat = f"{k}={v}"
+                if feat not in CrfFeatures.to_int_idx:
+                    CrfFeatures.to_int_idx[feat] = len(CrfFeatures.to_int_idx)
+        return res
+
+    @staticmethod
+    def get_numbered_features(X: list[list["CrfFeatures"]]) -> list[list[list[int]]]:
+        """特徴量を数値化する"""
+        res: list[list[list[int]]] = []
+        for word in X:
+            res_word: list[list[int]] = []
+            for mora in word:
+                res_mora: list[int] = [0] * len(CrfFeatures.to_int_idx)
+                for k, v in mora.model_dump().items():
+                    if type(v) in {int, float}:
+                        idx = CrfFeatures.to_int_idx[k]
+                        res_mora[idx] = int(v)
+                    elif type(v) is bool:
+                        idx = CrfFeatures.to_int_idx[k]
+                        res_mora[idx] = 1 if v else 0
+                    else:
+                        feat = f"{k}={v}"
+                        idx = CrfFeatures.to_int_idx[feat]
+                        res_mora[idx] = 1
+                res_word.append(res_mora)
+            res.append(res_word)
         return res
 
 
-class CrfLabel(list[str]):
-    NG = "NG"
-    B_ABBR = "B-Abbr"
-    I_ABBR = "I-Abbr"
+class CrfLabelSequence(list[str]):
+    def __init__(self, data: list[str] = []):
+        super().__init__(data)
 
     @classmethod
-    def from_abbreviation(cls, abbr: Abbreviation) -> "CrfLabel":
+    def from_abbreviation(cls, abbr: Abbreviation) -> "CrfLabelSequence":
         word_mora_list = [mora for elem in abbr.word_element_list for mora in elem.mora_list]
         abbr_mora_list = [mora for elem in abbr.abbreviation_element_list for mora in elem.mora_list]
         n = len(word_mora_list)
         abbr_n = len(abbr_mora_list)
         abbr_i = 0
-        res = [cls.NG] * n
+        res = [crf_label.NG] * n
         in_abbr = False
         for i in range(n):
             if abbr_i >= abbr_n:
                 break
             if word_mora_list[i] == abbr_mora_list[abbr_i]:
-                res[i] = cls.I_ABBR if in_abbr else cls.B_ABBR
+                res[i] = crf_label.I_ABBR if in_abbr else crf_label.B_ABBR
                 abbr_i += 1
                 in_abbr = True
             else:
