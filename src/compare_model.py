9231e03f150fc67a297edd34121e1b10f4956898
ayu0616 <ayumuogawa0616@gmail.com>
2024-06-19 15:31:50 +0900

    モデルのスコア比較プログラムを実装した

--- src/compare_model.py
+++ src/compare_model.py
@@ -6,10 +6,13 @@ import json
 import os
 
 import pandas as pd
+from lightgbm import LGBMClassifier
 from pydantic import BaseModel
 from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
 from sklearn_crfsuite import CRF
 
+from sequential_classfier import SequentialClassifier
 from type import Abbreviation, CrfFeatures, CrfLabelSequence, crf_label
 
 os.chdir(os.path.dirname(__file__))
@@ -25,10 +28,14 @@ class CaseScore(BaseModel):
 
     @property
     def precision(self):
+        if self.true_positive + self.false_positive == 0:
+            return 0
         return self.true_positive / (self.true_positive + self.false_positive)
 
     @property
     def recall(self):
+        if self.true_positive + self.false_negative == 0:
+            return 0
         return self.true_positive / (self.true_positive + self.false_negative)
 
     @property
@@ -55,12 +62,12 @@ def crf(
         score = CaseScore()
         for t, p in zip(true, pred):
             if t == p:
-                if t == crf_label.NG:
+                if p == crf_label.NG:
                     score.true_negative += 1
                 else:
                     score.true_positive += 1
             else:
-                if t == crf_label.NG:
+                if p == crf_label.NG:
                     score.false_positive += 1
                 else:
                     score.false_negative += 1
@@ -68,12 +75,54 @@ def crf(
     return score_list
 
 
-data_train, data_test = train_test_split(data, test_size=0.2)
-X_train = [*map(CrfFeatures.from_abbreviation, data_train)]
-y_train = list(map(CrfLabelSequence.from_abbreviation, data_train))
-X_test = [*map(CrfFeatures.from_abbreviation, data_test)]
-y_test = list(map(CrfLabelSequence.from_abbreviation, data_test))
+def sklearn_model(
+    model, X_train: list[list["CrfFeatures"]], y_train: list["CrfLabelSequence"], X_test: list[list["CrfFeatures"]], y_test: list["CrfLabelSequence"]
+) -> list[CaseScore]:
+    model.fit(X_train, y_train)
+    y_pred: list[CrfLabelSequence] = model.predict(X_test)
+    score_list: list[CaseScore] = []
+    for true, pred in zip(y_test, y_pred):
+        score = CaseScore()
+        for t, p in zip(true, pred):
+            if t == p:
+                if p == crf_label.NG:
+                    score.true_negative += 1
+                else:
+                    score.true_positive += 1
+            else:
+                if p == crf_label.NG:
+                    score.false_positive += 1
+                else:
+                    score.false_negative += 1
+        score_list.append(score)
+    return score_list
 
-crf_score_list = crf(X_train, y_train, X_test, y_test)
+
+crf_score_list: list[CaseScore] = []
+svm_score_list: list[CaseScore] = []
+lgbm_score_list: list[CaseScore] = []
+
+for i in range(10):
+    data_train, data_test = train_test_split(data, test_size=0.2)
+    X_train = [*map(CrfFeatures.from_abbreviation, data_train)]
+    y_train = list(map(CrfLabelSequence.from_abbreviation, data_train))
+    X_test = [*map(CrfFeatures.from_abbreviation, data_test)]
+    y_test = list(map(CrfLabelSequence.from_abbreviation, data_test))
+
+    crf_score_list.extend(crf(X_train, y_train, X_test, y_test))
+    svm_score_list.extend(
+        sklearn_model(SequentialClassifier(SVC(kernel="rbf", C=1, class_weight="balanced", gamma="auto", probability=True)), X_train, y_train, X_test, y_test)
+    )
+    lgbm_score_list.extend(sklearn_model(SequentialClassifier(LGBMClassifier()), X_train, y_train, X_test, y_test))
+
 df = pd.DataFrame({"f1": score.f1, "accuracy": score.accuracy, "recall": score.recall, "precision": score.precision} for score in crf_score_list)
+print("crf")
+print(df.describe())
+
+df = pd.DataFrame({"f1": score.f1, "accuracy": score.accuracy, "recall": score.recall, "precision": score.precision} for score in svm_score_list)
+print("svm")
+print(df.describe())
+
+df = pd.DataFrame({"f1": score.f1, "accuracy": score.accuracy, "recall": score.recall, "precision": score.precision} for score in lgbm_score_list)
+print("lgbm")
 print(df.describe())
