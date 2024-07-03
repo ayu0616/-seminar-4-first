e47511c08a521d945ead7c028d24026efcd70589
ayu0616 <ayumuogawa0616@gmail.com>
2024-07-03 16:46:54 +0900

    候補を出力する処理を実装した

--- src/model/sequential_classfier.py
+++ src/model/sequential_classfier.py
@@ -68,3 +68,37 @@ class SequentialClassifier:
                 yj = self.model.predict_proba([x[j]])[0]
                 y_proba.append(yj)
             yield y_proba
+
+    def predict_rank(self, X: list[list[CrfFeatures]], rank_n: int):
+        """予測確率の上位n位のラベルを返す"""
+        n = len(X)
+        class_list: list[str] = self.model.classes_
+        X_: list[list[list[int]]] = CrfFeatures.get_numbered_features(X)
+        for i in range(n):
+            x = X_[i]
+            m = len(x)
+            y_pred_proba: list[float] = [0.0]
+            y_pred: list[CrfLabelSequence] = [CrfLabelSequence([])]
+            for j in range(m):
+                y_pred_proba_next: list[float] = []
+                y_pred_next: list[CrfLabelSequence] = []
+                proba_memo: dict[str, np.ndarray[float]] = {}
+                for y, p in zip(y_pred, y_pred_proba):
+                    label = ""
+                    if j > 0:
+                        label = y_pred[0][j - 1]
+                        idx = CrfFeatures.to_int_idx[label]
+                        x[j][idx] = 1
+                    if label not in proba_memo:
+                        y_proba = self.model.predict_proba([x[j]])[0]
+                        proba_memo[label] = y_proba
+                    else:
+                        y_proba = proba_memo[label]
+                    for k, next_p in enumerate(y_proba):
+                        class_name = class_list[k]
+                        y_pred_next.append(y + [class_name])
+                        y_pred_proba_next.append(p + next_p)
+                index_list = np.argsort(y_pred_proba_next)[::-1][:rank_n]
+                y_pred = [y_pred_next[i] for i in index_list]
+                y_pred_proba = [y_pred_proba_next[i] for i in index_list]
+            yield y_pred
